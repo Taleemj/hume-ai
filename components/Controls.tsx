6a42618855079f9cf61dc41188a591eece21c97a
Taleem <taleemmankuer@gmail.com>
2024-07-27 17:29:00 +0100

    update: captions

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState, useEffect, useRef } from "react";
 import { useVoice, VoiceReadyState } from "@humeai/voice-react";
 import { ImPhoneHangUp } from "react-icons/im";
 import { FaPhoneAlt } from "react-icons/fa";
@@ -6,15 +7,42 @@ import { FaMicrophone, FaMicrophoneSlash, FaRegClosedCaptioning } from "react-ic
 import Image from "next/image";
 
 export default function Controls() {
-  const { connect, disconnect, readyState, mute, isMuted, unmute } = useVoice();
+  const { connect, disconnect, readyState, mute, isMuted, unmute, messages } = useVoice();
+  const [isCaptioning, setIsCaptioning] = useState(false);
+  const captionsRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (captionsRef.current) {
+      captionsRef.current.scrollTop = captionsRef.current.scrollHeight;
+    }
+  }, [messages]);
 
   if (readyState === VoiceReadyState.OPEN) {
     return (
-      <div className="w-full h-[100vh] flex d items-center justify-center relative  bg-[#272727]">
+      <div className="w-full h-[100vh] flex flex-col items-center justify-center relative  bg-[#272727]">
         <div className="w-[150px] h-[150px] bg-[#414141] rounded-full grid place-items-center">
           <Image src="/Hume Ai.png" alt="logo" width={170} height={170} />
         </div>
 
+        <div
+          className={`w-full mx-auto mt-5 text-white overflow-y-scroll captions my-3 transition-all duration-300 ${
+            isCaptioning ? "h-[40%] md:h-[250px] p-4" : "h-0 p-0"
+          }`}
+          ref={captionsRef}
+        >
+          {messages.map((msg, index) => {
+            if (msg.type === "user_message" || msg.type === "assistant_message") {
+              return (
+                <div className="text-[17px] py-1" key={msg.type + index}>
+                  <div className="font-bold">{msg.message.role == "user" ? "You" : "EVI"}</div>
+                  <div>{msg.message.content}</div>
+                </div>
+              );
+            }
+
+            return null;
+          })}
+        </div>
         <div
           className={
             "flex items-center justify-between w-[75%] md:w-[20%] mx-auto fixed bottom-[3%] left-1/2 transform -translate-x-1/2"
@@ -28,7 +56,12 @@ export default function Controls() {
           >
             {isMuted ? <FaMicrophoneSlash /> : <FaMicrophone />}
           </button>
-          <button className="bg-[#414141] hover:bg-[#555555] text-white font-bold py-2 px-6 w-[80px] h-[55px] grid place-items-center rounded-full text-[26px]">
+          <button
+            onClick={() => setIsCaptioning(!isCaptioning)}
+            className={` ${
+              isCaptioning ? "bg-blue-600 hover:bg-blue-500" : "bg-[#414141] hover:bg-[#555555]"
+            } text-white font-bold py-2 px-6 w-[80px] h-[55px] grid place-items-center rounded-full text-[26px]`}
+          >
             <FaRegClosedCaptioning />
           </button>
           <button
